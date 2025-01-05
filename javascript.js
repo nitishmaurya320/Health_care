850fa00604bfe6acbc7b04d0d6d13d96644cac56
Nitish Maurya <nitishmaurya2255@gmail.com>
2025-01-05 16:22:45 +0530

    remark of bmi added

--- javascript.js
+++ javascript.js
@@ -10,7 +10,18 @@ document.getElementById('caluu').addEventListener("click", function(){
   
     let ans=weight.value/(height.value/100*height.value/100);
     document.querySelector('.ans').innerHTML=ans.toFixed(2);
-    
+
+    if(ans<18.4){
+        document.querySelector('.remark').innerHTML="Under weight";
+    }
+    else if(ans>18.4&&ans<24.9){
+        document.querySelector('.remark').innerHTML="Normal";
+    }
+    else if(ans>25.0&&ans<39.9){
+        document.querySelector('.remark').innerHTML="Normal";
+    }   
+    else
+    document.querySelector('.remark').innerHTML="Obese";
 });
 
 
